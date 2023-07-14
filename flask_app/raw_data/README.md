f9239695aa0be82847e9bcced9f7d80d611b8b26
Ram S <ram.senth@berkeley.edu>
2023-07-14 06:34:32 -0700

    APIs and new dataset for pandemic theme

--- flask_app/raw_data/README.md
+++ flask_app/raw_data/README.md
@@ -1,15 +1,22 @@
 # Data Files
 ## Immunization Data
+**wuenic2021rev_data_2023-07-13.csv** - [UNICEF Vaccination Coverage % and Numbers](https://unicef.shinyapps.io/wuenic-analytics-2022/)
+
 **cases-of-paralytic-polio-from-vaccine-derived-viruses-by-strain.csv** - File with number of polio cases resulting from vaccines
 
 **unicef_dtp_first_shot_coverage_2010_2023.csv** - DTP first shot coverage for all countries and WHO regions, from [UNICEF immunization data](https://data.unicef.org/resources/data_explorer/unicef_f/?ag=UNICEF&df=GLOBAL_DATAFLOW&ver=1.0&dq=AFG+ALB+DZA+ASM+AND+AGO+AIA+ATG+ARG+ARM+ABW+AUS+AUT+AZE+BHS+BHR+BGD+BRB+BLR+BEL+BLZ+BEN+BMU+BTN+BOL+BES+BIH+BWA+BRA+VGB+BRN+BGR+BFA+BDI+CPV+KHM+CMR+CAN+CYM+CAF+TCD+CHI+CHL+HKG+CHN+COL+COM+COG+COK+CRI+CIV+HRV+CUB+CUW+CYP+CZE+PRK+COD+DNK+DJI+DMA+DOM+ECU+EGY+SLV+GNQ+ERI+EST+SWZ+ETH+FLK+FRO+FJI+FIN+FRA+GUF+PYF+GAB+GMB+GEO+DEU+GHA+GIB+GRC+GRL+GRD+GLP+GUM+GTM+GNB+GIN+GUY+HTI+VAT+HND+HUN+ISL+IND+IDN+IRN+IRQ+IRL+IMN+ISR+ITA+JAM+JPN+JOR+KAZ+KEN+KIR+XKX+KWT+KGZ+LAO+LVA+LBN+LSO+LBR+LBY+LIE+LTU+LUX+MAC+MDG+MWI+MYS+MDV+MLI+MLT+MHL+MTQ+MRT+MUS+MYT+MEX+FSM+MCO+MNG+MNE+MSR+MAR+MOZ+MMR+NAM+NRU+NPL+NLD+NCL+NZL+NIC+NER+NGA+MKD+NIU+MNP+NOR+OMN+TWN+PAK+PLW+PAN+PNG+PRY+PER+PHL+POL+PRT+PRI+QAT+KOR+MDA+REU+ROU+RUS+RWA+BLM+SHN+KNA+LCA+SXM+MAF+SPM+VCT+WSM+SMR+STP+SAU+SEN+SRB+SYC+SLE+SGP+SVK+SVN+SLB+SOM+ZAF+SSD+ESP+LKA+PSE+SDN+SUR+SWE+CHE+SYR+TJK+THA+TLS+TGO+TKL+TON+TTO+TUN+TUR+TKM+TCA+TUV+UGA+UKR+ARE+GBR+TZA+USA+URY+UZB+VUT+VEN+VNM+VIR+WLF+YEM+ZMB+ZWE+WHO_REG_GLOBAL+WHO_AFRO+WHO_AMRO+WHO_EMRO+WHO_EURO+WHO_SEARO+WHO_WPRO.IMMUNISATION+IM_DTP1..&startPeriod=2010&endPeriod=2020&lastnobservations=1)
 
+UNICEF round 2 (all vaccines + demo + economy ) - https://data.unicef.org/resources/data_explorer/unicef_f/?ag=UNICEF&df=GLOBAL_DATAFLOW&ver=1.0&dq=AFG+ALB+DZA+ASM+AND+AGO+AIA+ATG+ARG+ARM+ABW+AUS+AUT+AZE+BHS+BHR+BGD+BRB+BLR+BEL+BLZ+BEN+BMU+BTN+BOL+BES+BIH+BWA+BRA+VGB+BRN+BGR+BFA+BDI+CPV+KHM+CMR+CAN+CYM+CAF+TCD+CHI+CHL+HKG+CHN+COL+COM+COG+COK+CRI+CIV+HRV+CUB+CUW+CYP+CZE+PRK+COD+DNK+DJI+DMA+DOM+ECU+EGY+SLV+GNQ+ERI+EST+SWZ+ETH+FLK+FRO+FJI+FIN+FRA+GUF+PYF+GAB+GMB+GEO+DEU+GHA+GIB+GRC+GRL+GRD+GLP+GUM+GTM+GNB+GIN+GUY+HTI+VAT+HND+HUN+ISL+IND+IDN+IRN+IRQ+IRL+IMN+ISR+ITA+JAM+JPN+JOR+KAZ+KEN+KIR+XKX+KWT+KGZ+LAO+LVA+LBN+LSO+LBR+LBY+LIE+LTU+LUX+MAC+MDG+MWI+MYS+MDV+MLI+MLT+MHL+MTQ+MRT+MUS+MYT+MEX+FSM+MCO+MNG+MNE+MSR+MAR+MOZ+MMR+NAM+NRU+NPL+NLD+NCL+NZL+NIC+NER+NGA+MKD+NIU+MNP+NOR+OMN+TWN+PAK+PLW+PAN+PNG+PRY+PER+PHL+POL+PRT+PRI+QAT+KOR+MDA+REU+ROU+RUS+RWA+BLM+SHN+KNA+LCA+SXM+MAF+SPM+VCT+WSM+SMR+STP+SAU+SEN+SRB+SYC+SLE+SGP+SVK+SVN+SLB+SOM+ZAF+SSD+ESP+LKA+PSE+SDN+SUR+SWE+CHE+SYR+TJK+THA+TLS+TGO+TKL+TON+TTO+TUN+TUR+TKM+TCA+TUV+UGA+UKR+ARE+GBR+TZA+USA+URY+UZB+VUT+VEN+VNM+VIR+WLF+YEM+ZMB+ZWE+WHO_REG_GLOBAL+WHO_AFRO+WHO_AMRO+WHO_EMRO+WHO_EURO+WHO_SEARO+WHO_WPRO.IMMUNISATION+IM_DTP1+ED_ATTND_FRML_INST+ED_15-24_LR+ECON_GVT_HLTH_EXP_PTEXP+ECON_GVT_HLTH_EXP_PTGDP+DM_POP_TOT+DM_POP_U5+CME_MRY0T4+CME_COVID_DEATHS+CME_COVID_CASES+IM_DTP3+IM_HEPB3+IM_HEPBB+IM_HIB3+IM_IPV1+IM_MCV1+IM_MCV2+IM_PAB+IM_PCV3+IM_POL3+IM_RCV1+IM_ROTAC+PV_CHLD_DPRV-L1-HS+PV_CHLD_DPRV-L5-HS+PV_CHLD_INCM-PL+PV_LEVEL+PV_GAP..&startPeriod=2010&endPeriod=2023
+
 **unicef_dtp_first_shot_coverage_2010_2023_summary.csv** - Summary from DTP first shot coverage, with difference beween coverage in 2020 vs that in 2019 for all countries.
 
-## Country Data 
+**fusion_GLOBAL_DATAFLOW_UNICEF_1.0_.DM_BRTS+DM_POP_U5+DM_POP_TOT.F+M+_T.csv** - [UNICEF pupolation data](
+https://data.unicef.org/resources/data_explorer/unicef_f/?ag=UNICEF&df=GLOBAL_DATAFLOW&ver=1.0&dq=.DM_BRTS+DM_POP_U5+DM_POP_TOT.F+M+_T&startPeriod=2013&endPeriod=2023)
+
+## Country Data
 **countries_master.csv** - List of all countries, their various codes as well as their WHO region. Synthsized from Wikipedia and WHO data listed below.
 
-**who_regions_master.csv** - List of WHO regions from WHO, derived from [Wikipedia data](https://en.wikipedia.org/wiki/List_of_WHO_regions) 
+**who_regions_master.csv** - List of WHO regions from WHO, derived from [Wikipedia data](https://en.wikipedia.org/wiki/List_of_WHO_regions)
 
 **who_countries.csv** - Mapping of country to WHO region, derived from [Wikipedia data](https://en.wikipedia.org/wiki/List_of_WHO_regions)
 
--- flask_app/w209.py
+++ flask_app/w209.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import pandas as pd
 import os
 import json
+from flask_cors import CORS
+import sys
+
+PROD_BASE_DATA_FOLDER = 'w209/raw_data'
+DEV_BASE_DATA_FOLDER = 'raw_data'
 
 app = Flask(__name__)
+CORS(app)
 
-BASE_DATA_FOLDER = 'w209/data'
+if 'FLASK_ENV' in os.environ and os.environ['FLASK_ENV'] == 'development':
+    app.config['BASE_DATA_FOLDER'] = DEV_BASE_DATA_FOLDER
+else:
+    app.config['BASE_DATA_FOLDER'] = PROD_BASE_DATA_FOLDER
+
+UNICEF_VACC_DATA_FILE = 'wuenic2021rev_data_2023-07-13.csv'
 COUNTRY_ISO_CODES_FILE = 'wikipedia-iso-country-codes.csv'
-WHO_REGIONS_FILE = 'who_regions.csv'
 WHO_COUNTRIES_FILE = 'who_countries.csv'
+WHO_REGIONS_FILE = 'who_regions.csv'
 UNICEF_DTP_DATA_FILE = 'unicef_dtp_first_shot_coverage_2010_2023.csv'
+VACCINES_MASTER_FILE = 'vaccines_master.csv'
 
 @app.route("/")
 def w209():
@@ -25,10 +37,77 @@ def api():
 def debug_info():
     return os.getcwd()
 
+@app.route("/vaxviz/pandemic/v2/vaxrates/whoregions", methods = ['GET'])
+def v2_vax_rates_who_regions():
+    try:
+        df = helper_countries_vax_data_v2(True)
+    except Exception as e:
+        return json.dumps({ 'error': f'Failed to verify: {e}' }), 500
+    df = df.drop(columns=['Coverage', 'name', 'key_0', 'region_number', 'region_subcode', 'numeric_code', 'iso_3166_2'])
+
+    # Create region level summary
+    df_regional = df.groupby(['region_code', 'Year', 'Vaccine']).agg({'Vaccinated':'sum','Target':'sum'}).reset_index()
+
+
+    # Create global summary
+    df_global = df_regional.groupby(['Year', 'Vaccine']).agg({'Vaccinated':'sum','Target':'sum'}).reset_index()
+    df_global['region_code'] = 'REG_GLOBAL'
+    df_merged = pd.concat([df_regional, df_global], ignore_index=True, verify_integrity=True)
+    df_merged['Coverage'] = df_merged['Vaccinated'] / df_merged['Target'] * 100
+    # df_merged['Coverage'] = df_merged['Coverage'].astype(int)
+    df_merged['Not Covered'] = (df_merged['Target'] - df_merged['Vaccinated']) / df_merged['Target'] * 100
+    # df_merged['Not Covered'] = df_merged['Not Covered'].astype(int)
+
+    # Add region name.
+    df_regions = helper_regions_master()
+    df_merged = pd.merge(df_merged, df_regions, left_on=df_merged["region_code"], right_on=df_regions["code"], how='left')
+    df_missing_code = df_merged[(df_merged['region_name'].isna())]
+    if df_missing_code['region_code'].unique().size > 0:
+        return json.dumps({ 'error': f"Could not identify codes for {df_missing_code['region_code'].unique().tolist()}."}), 500
+    df_merged = df_merged.drop(columns=['key_0', 'code', 'key_in_data'])
+    return df_merged.to_csv();
+
+
+
+
+@app.route("/vaxviz/pandemic/v2/vaxrates/countries", methods = ['GET'])
+def v2_vax_rates_countries():
+    """
+        Returns the annual coverage rate as well as actual numbers for each country against each vaccine from 2015 to 2021.
+        Vaccines included: BCG, DTP1, DTP3, Hepb3, Hepbb, Hib3, IPV1, MCV1, MCV2, PCV3, Pol3, Rotac
+        Accepts query string parameter pivot_on_year. If value is true then the yearly values are converted to columns.
+    """
+    if 'pivot_on_year' in request.args:
+        pivot_on_year = (request.args['pivot_on_year'].lower() == 'true')
+    else:
+        pivot_on_year = False
+
+    try:
+        df = helper_countries_vax_data_v2(True)
+        df = df[['Name', 'alpha_3_code', 'Vaccine', 'Year', 'Coverage', 'Vaccinated', 'Target']]
+    except Exception as e:
+        return json.dumps({ 'error': f'Failed to verify: {e}' }), 500
+
+    # Use pivot to convert the yearly values to columns.
+    if pivot_on_year:
+        df = pd.pivot_table(df, index=['Name', 'alpha_3_code', 'Vaccine'], columns=['Year'], values=['Coverage', 'Vaccinated', 'Target'])
+        df.columns = [ '_'.join([str(c) for c in c_list]) for c_list in df.columns.values ]
+        out_filename = 'unicef_national_coverage_2015_2021_pivot.csv'
+    else:
+        out_filename = 'unicef_national_coverage_2015_2021.csv'
+
+    return Response(
+       df.to_csv(),
+       mimetype="text/csv",
+       headers={"Content-disposition":
+       f"attachment; filename={out_filename}"})
+
+
+
 @app.route("/vaxviz/pandemic/v1/national_change/")
 def v1_natl_change():
     # Ensure the data folder and its contents are uploaded to server before calling this endpoint
-    df = pd.read_csv(f'{BASE_DATA_FOLDER}/{UNICEF_DTP_DATA_FILE}')
+    df = pd.read_csv(f'{app.config["BASE_DATA_FOLDER"]}/{UNICEF_DTP_DATA_FILE}')
     df_countries = df[(~df['REF_AREA:Geographic area'].str.startswith('WHO')) & ((df['TIME_PERIOD:Time period'] >= 2018))]
     df_pivoted = df_countries.pivot_table('OBS_VALUE:Observation Value', ['REF_AREA:Geographic area'], 'TIME_PERIOD:Time period')
     df_pivoted['delta'] = df_pivoted[2020] - df_pivoted[2019]
@@ -39,20 +118,14 @@ def v1_natl_change():
        headers={"Content-disposition":
        "attachment; filename=unicef_dtp_first_shot_coverage_2010_2023_summary.csv"})
 
-@app.route("/vaxviz/countries_csv/")
-def countries_csv():
-    # Ensure the data folder and its contents are uploaded to server before calling this endpoint
-    df_country_iso_codes = pd.read_csv(f'{BASE_DATA_FOLDER}/{COUNTRY_ISO_CODES_FILE}')
-    df_who_countries = pd.read_csv(f'{BASE_DATA_FOLDER}/{WHO_COUNTRIES_FILE}')
-
-    # Add alpha-3 code to who_countries
-    df_countries_with_alpha_3 = pd.merge(df_who_countries,df_country_iso_codes, left_on=df_who_countries["name"].str.lower(), right_on=df_country_iso_codes["name"].str.lower(), how='left')
-    df_countries_with_alpha_3['name'] = df_countries_with_alpha_3['name_x']
-    df_countries_with_alpha_3 = df_countries_with_alpha_3.drop(columns=['key_0', 'name_y', 'name_x'])
+@app.route("/vaxviz/countries_master/")
+def countries_master():
+    # Load countries master
+    df_countries_with_alpha_3 = helper_countries_master()
 
     # Verification to ensure that we have all countries covered
     try:
-        df_immunization_data = pd.read_csv(f'{BASE_DATA_FOLDER}/{UNICEF_DTP_DATA_FILE}')
+        df_immunization_data = pd.read_csv(f'{app.config["BASE_DATA_FOLDER"]}/{UNICEF_DTP_DATA_FILE}')
         # Extract country code from REF_AREA:Geographic area col in the immunization data
         df_immunization_data['country_code'] = df_immunization_data['REF_AREA:Geographic area'].str.split(": ", expand=True, n=1)[0].astype('string')
         df_merged = pd.merge(df_immunization_data,df_countries_with_alpha_3, left_on=df_immunization_data["country_code"], right_on=df_countries_with_alpha_3["alpha_3_code"], how='left')
@@ -68,5 +141,50 @@ def countries_csv():
        mimetype="text/csv",
        headers={"Content-disposition":"attachment; filename=countries_master.csv"})
 
+@app.route("/vaxviz/vaccines_master/")
+def vaccines_master():
+    df = pd.read_csv(f'{app.config["BASE_DATA_FOLDER"]}/{VACCINES_MASTER_FILE}')
+    return Response(
+       df.to_csv(index=False),
+       mimetype="text/csv",
+       headers={"Content-disposition":"attachment; filename=vaccines_master.csv"})
+
+def helper_countries_vax_data_v2(merge_with_countries_master):
+    # Read the raw data
+    df = pd.read_csv(f'{app.config["BASE_DATA_FOLDER"]}/{UNICEF_VACC_DATA_FILE}', usecols=['Name', 'Year', 'Vaccine', 'Coverage', 'Vaccinated', 'Target'])
+
+    # Set correct datatype for numeric values
+    df['Coverage'] = pd.to_numeric(df['Coverage'])
+    df['Vaccinated'] = pd.to_numeric(df['Vaccinated'].str.replace(',', '').str.replace('-', '').str.replace('<', ''))
+    df['Target'] = pd.to_numeric(df['Target'].str.replace(',', '').str.replace('-', '').str.replace('<', ''))
+
+    if merge_with_countries_master:
+        df_countries = helper_countries_master()
+        df_merged = pd.merge(df, df_countries, left_on=df['Name'], right_on=df_countries["name"], how='left')
+        df_merged.T.drop_duplicates()
+        df_missing_code = df_merged[(df_merged['alpha_3_code'].isna())]
+        if df_missing_code['Name'].unique().size > 0:
+            raise Exception(f"Could not identify codes for {df_missing_code['Name'].unique().tolist()}")
+        return df_merged
+
+    return df
+
+def helper_regions_master():
+    df_regions = pd.read_csv(f'{app.config["BASE_DATA_FOLDER"]}/{WHO_REGIONS_FILE}')
+    return df_regions
+
+
+def helper_countries_master():
+    df_country_iso_codes = pd.read_csv(f'{app.config["BASE_DATA_FOLDER"]}/{COUNTRY_ISO_CODES_FILE}')
+    df_who_countries = pd.read_csv(f'{app.config["BASE_DATA_FOLDER"]}/{WHO_COUNTRIES_FILE}')
+
+    # Add alpha-3 code to who_countries
+    df_countries_with_alpha_3 = pd.merge(df_who_countries,df_country_iso_codes, left_on=df_who_countries["name"].str.lower(), right_on=df_country_iso_codes["name"].str.lower(), how='left')
+    df_countries_with_alpha_3['name'] = df_countries_with_alpha_3['name_x']
+    df_countries_with_alpha_3 = df_countries_with_alpha_3.drop(columns=['key_0', 'name_y', 'name_x'])
+    return df_countries_with_alpha_3
+
+
 if __name__ == "__main__":
+    #env should be dev for local work
     app.run()
